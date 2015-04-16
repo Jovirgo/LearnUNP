a10969b67a06b1da8a155f713d9b11f3bd05c4c3
Jovirgo <ojoe23@gmail.com>
2015-04-16 22:14:15 +0800

    Initial DayTimeTcp

---
+++ DayTimeTcpCli/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DayTimeTcpCli
+//
+//  Created by 欧洲 on 15/4/16.
+//  Copyright (c) 2015年 OJO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
